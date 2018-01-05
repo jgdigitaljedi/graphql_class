060df72a32de5a289d57fa1fa36e7a0e3dfc9af2
Joey Gauthier <jgauthier@r4.co>
2018-01-05 08:15:50 -0600

    express server, graphiql, course finished

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
+
+console.log('starting the server');
+
+app.listen(4000);
